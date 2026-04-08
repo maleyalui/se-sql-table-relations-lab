606198a52adb84d410b3eec0557a8929da67868a
maleyalui <maleyalui1@gmail.com>
2026-04-08 19:46:57 +0300

    Initial commit

--- main.py
+++ main.py
@@ -11,42 +11,151 @@ pd.read_sql("""SELECT * FROM sqlite_master""", conn)
 
 # STEP 1
 # Replace None with your code
-df_boston = None
+df_boston = pd.read_sql("""
+                        SELECT e.firstName,
+                        e.lastName
+                        FROM employees e
+                        JOIN offices o
+                        ON e.officeCode = o.officeCode
+                        WHERE o.city = 'Boston'
+                        """, conn)
 
 # STEP 2
 # Replace None with your code
-df_zero_emp = None
+df_zero_emp = pd.read_sql("""
+                          SELECT o.officeCode, o.city, o.state
+                          FROM offices o
+                          LEFT JOIN employees e
+                          ON o.officeCode = e.officeCode
+                          WHERE e.employeeNumber IS NULL
+                          """, conn)
 
 # STEP 3
 # Replace None with your code
-df_employee = None
+df_employee = pd.read_sql("""
+                          SELECT e.firstName, e.lastName, o.city, o.state
+                          FROM employees e
+                          LEFT JOIN offices o
+                          ON e.officeCode = o.officeCode
+                          ORDER BY e.firstName, e.lastName
+                          """, conn)
 
 # STEP 4
 # Replace None with your code
-df_contacts = None
+df_contacts = pd.read_sql("""
+                          SELECT c.contactFirstName,
+                                 c.contactLastName,
+                                 c.phone,
+                                 c.salesRepEmployeeNumber
+                          FROM customers c
+                          LEFT JOIN orders o
+                          ON c.customerNumber = o.customerNumber
+                          WHERE o.orderNumber IS NULL
+                          ORDER BY c.contactLastName
+                          """, conn)
 
 # STEP 5
 # Replace None with your code
-df_payment = None
-
+df_payment = pd.read_sql("""
+SELECT c.contactFirstName,
+       c.contactLastName,
+       p.amount,
+       p.paymentDate
+FROM customers c
+JOIN payments p
+ON c.customerNumber = p.customerNumber
+ORDER BY CAST(p.amount AS REAL) DESC
+""", conn)
 # STEP 6
 # Replace None with your code
-df_credit = None
+df_credit = pd.read_sql("""
+                        SELECT e.employeeNumber,
+                                e.firstName,
+                                e.lastName,
+                                COUNT(c.customerNumber) as numcustomers
+                        FROM employees e
+                        JOIN customers c
+                        ON e.employeeNumber = c.salesRepEmployeeNumber
+                        GROUP BY e.employeeNumber
+                        HAVING AVG(c.creditLimit) > 90000
+                        ORDER BY numcustomers DESC
+                        """, conn)
 
 # STEP 7
 # Replace None with your code
-df_product_sold = None
+df_product_sold = pd.read_sql("""
+                              SELECT p.productName,
+                              COUNT(od.orderNumber) as numorders,
+                              SUM(od.quantityOrdered) as totalunits
+                              FROM products p
+                              JOIN orderdetails od
+                              ON p.productCode = od.productCode
+                              GROUP BY p.productName
+                              ORDER BY totalunits DESC
+                              """, conn)
 
 # STEP 8
 # Replace None with your code
-df_total_customers = None
+df_total_customers = pd.read_sql("""
+                                 SELECT p.productName,
+                                          p.productCode,
+                                          COUNT(DISTINCT o.customerNumber) as numpurchasers
+                                   FROM products p
+                                   JOIN orderdetails od
+                                   ON p.productCode = od.productCode
+                                   JOIN orders o
+                                   ON od.orderNumber = o.orderNumber
+                                   GROUP BY p.productCode
+                                   ORDER BY numpurchasers DESC
+                                   """, conn)
 
 # STEP 9
 # Replace None with your code
-df_customers = None
+df_customers = pd.read_sql("""
+       SELECT o.officeCode,
+       o.city,
+       COUNT(c.customerNumber) as n_customers
+       FROM offices o
+       LEFT JOIN employees e
+       ON o.officeCode = e.officeCode
+       LEFT JOIN customers c
+       ON e.employeeNumber = c.salesRepEmployeeNumber
+       GROUP BY o.officeCode
+       """,conn)
 
 # STEP 10
 # Replace None with your code
-df_under_20 = None
+df_under_20 = pd.read_sql("""
+SELECT DISTINCT e.employeeNumber,
+       e.firstName,
+       e.lastName,
+       o.city,
+       o.officeCode
+FROM employees e
+JOIN offices o
+  ON e.officeCode = o.officeCode
+JOIN customers c
+  ON e.employeeNumber = c.salesRepEmployeeNumber
+JOIN orders ord
+  ON c.customerNumber = ord.customerNumber
+JOIN orderdetails od
+  ON ord.orderNumber = od.orderNumber
+WHERE od.productCode IN (
+    SELECT p.productCode
+    FROM products p
+    JOIN orderdetails od
+      ON p.productCode = od.productCode
+    JOIN orders o
+      ON od.orderNumber = o.orderNumber
+    GROUP BY p.productCode
+    HAVING COUNT(DISTINCT o.customerNumber) < 20
+)
+ORDER BY CASE e.firstName
+            WHEN 'Loui' THEN 0
+            ELSE 1
+         END,
+         e.firstName,
+         e.lastName
+""", conn)
 
 conn.close()
